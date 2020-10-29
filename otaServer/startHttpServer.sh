088a555cf0162a0953048a5084cf99daebffda89
Axel Ruder <axel.ruder@gmail.com>
2020-10-29 22:47:18 +0100

    added ota script

--- otaServer/startHttpServer.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run -d -p 8080:80 -v /Users/axel/Documents/Projects/Arduino/espVent/otaServer/data:/usr/share/nginx/html:ro nginx:stable 
--- otaServer/startUpdate.sh
+++
@@ -1,2 +0,0 @@
-cp ../.pio/build/esp-wrover-kit/firmware.bin data/
-mosquitto_pub -h 192.168.178.27 -t cmnd/espVent/ota -m "url:http://192.168.178.20:8080/firmware.bin,md5:xy"
